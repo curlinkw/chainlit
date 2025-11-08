dfc2d37c7ea010e0b7afb169b31cb3388a0908b6
curlinkw <curlinkw@gmail.com>
2025-11-08 17:43:00 +0300

    change: no need for healthchecks

--- backend/chainlit/langchain/_checkpointer_setup.py
+++ backend/chainlit/langchain/_checkpointer_setup.py
@@ -10,10 +10,6 @@ def setup(checkpointer_conn_string: str | None):
         ) as checkpointer:
             checkpointer.setup()
 
-        if (status_file := os.getenv("CHECKPOINTER_STATUS_FILE")) is not None:
-            with open(status_file, "w") as f:
-                f.write("healthy")
-
 
 if __name__ == "__main__":
     setup(checkpointer_conn_string=os.getenv("CHECKPOINT_DATABASE_URL"))
